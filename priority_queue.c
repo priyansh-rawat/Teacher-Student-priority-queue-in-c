089783c6b3d149018566c646009b71bd7f1a5f8c
priyansh-rawat <61158875+priyansh-rawat@users.noreply.github.com>
2020-04-11 03:11:24 +0530

    Program modified to work on system calls.

--- priority_queue.c
+++ priority_queue.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+
 
 int ip1(int a[],int total)
 {
@@ -24,8 +27,8 @@ int priority_t(int t_point,int t_total,float tw[],float tb[],int t[],int s_point
 	{
 		if((int)tw[t_point]>100)
 		{
-			printf("Teacher%d will be served.\t",t[t_point]+1);
-			printf("Waiting time: %f\n",tw[t_point]);
+			printf("\nTeacher%d will be served.\t",t[t_point]+1);
+			printf("Waiting time: %.2f\n",tw[t_point]);
 			x=tb[t_point];
 			t_point++;
 		
@@ -36,8 +39,8 @@ int priority_t(int t_point,int t_total,float tw[],float tb[],int t[],int s_point
 		else break;
 	}
 	return count;
-}
-	
+}	
+
 int priority_s(int t_point,int t_total,float tw[],int s_point,int s_total,float sw[],int s[],float sb[])
 {	
 	int i,x,count=0;
@@ -45,8 +48,8 @@ int priority_s(int t_point,int t_total,float tw[],int s_point,int s_total,float 
 	{
 		if((int)sw[s_point]>100)
 		{
-			printf("Student%d will be served.\t",s[s_point]+1);
-			printf("Waiting time: %f\n",sw[s_point]);
+			printf("\nStudent%d will be served.\t",s[s_point]+1);
+			printf("Waiting time: %.2f\n",sw[s_point]);
 			x=sb[s_point];
 			s_point++;
 			op1(t_point,t_total,tw,x);
@@ -57,46 +60,50 @@ int priority_s(int t_point,int t_total,float tw[],int s_point,int s_total,float 
 	}
 	return count;
 }
+		
+
 
 int main()
 {
 	int s[25],t[25];
 	float sb[25],tb[25],*sw,*tw;	
 	int s_total=0,t_total=0,s_point=0,t_point=0;
-	char c;
+	char c[2];
 	int choice,i,x,check=0,y;
-	
+	int z1;
+	char buffer[10];	
+
 	sw=(float*)calloc(25,sizeof(float));
 	tw=(float*)calloc(25,sizeof(float));
 	
-	printf("WELCOME\n");
+	write(1,"WELCOME\n",strlen("WELCOME\n"));
 	while(check==0)
 	{
-		printf("Would you like to take input?(y/n) ");
-		scanf("%s",&c);
-		while(c=='y')
-		{
-			printf("Enter the details:\n"); 
-			printf("Student/Teacher? (0/1) :");
-			scanf("%d",&choice);
+		write(1,"Would you like to take input?(y/n) ",strlen("Would you like to take input?(y/n) "));
+		z1=read(0,c,sizeof(c));
+		while(c[0]=='y')
+		{		
+			write(1,"Enter the details:\n",strlen("Enter the details:\n"));
+			write(1,"Student/Teacher? (0/1) :",strlen("Student/Teacher? (0/1) :"));
+			z1=read(0,buffer,sizeof(buffer));
+			choice=atoi(buffer);
 			switch(choice)
 			{
 				case 0 :	s_total=ip1(s,s_total);
-							printf("Enter burst time: ");
-							scanf("%f",&sb[s_total-1]);
-							break;
-							
+						write(1,"Enter burst time: ",strlen("Enter burst time: "));
+						scanf("%f",&sb[s_total-1]);
+						break;
+
 				case 1 : 	t_total=ip1(t,t_total);
-							printf("Enter burst time");
-							scanf("%f",&tb[t_total-1]);
-							break;		
-						
+						write(1,"Enter burst time: ",strlen("Enter burst time: "));
+						scanf("%f",&tb[t_total-1]);
+						break;
 				default: printf("wrong input\n");	
 			}
-			printf("would you like to take more inputs:(y/n) ");
-			scanf("%s",&c);			
+			write(1,"Would you like to take more input?(y/n) ",strlen("Would you like to take more input?(y/n) "));
+			z1=read(0,c,sizeof(c));				
 		}
-		
+			
 		if(t_point<t_total)
 		{
 			if((int)tw[t_point]>100)
@@ -105,6 +112,7 @@ int main()
 				t_point=t_point+y;
 			}
 		}
+
 		if(s_point<s_total)
 		{
 			if((int)sw[s_point]>100)
@@ -113,11 +121,11 @@ int main()
 				s_point=s_point+y;
 			}
 		}
-		
+
 		if(t_point<t_total)	
 		{
-			printf("Teacher%d will be served.\t",t[t_point]+1);
-			printf("Waiting time: %f\n",tw[t_point]);
+			printf("\nTeacher%d will be served.\t",t[t_point]+1);
+			printf("Waiting time: %.2f\n",tw[t_point]);
 			x=tb[t_point];
 			t_point++;
 			
@@ -126,21 +134,19 @@ int main()
 		}
 		else if(t_point==t_total && s_point<s_total)
 		{
-			printf("Student%d will be served.\t",s[s_point]+1);
-			printf("Waiting time: %f\n",sw[s_point]);
+			printf("\nStudent%d will be served.\t",s[s_point]+1);
+			printf("Waiting time: %.0f\n",sw[s_point]);
 			x=sb[s_point];
 			s_point++;
 			op1(s_point,s_total,sw,x);
 		}
 		else
 		{
-			printf("\nmess empty\n");
+			write(1,"\nmess empty\n",strlen("\nmess empty\n"));	
 			check=1;
 		}
-		c='y';	
 	}
-
 	free(sw);	
 	free(tw);
-return 0;
+	return 0;
 }
